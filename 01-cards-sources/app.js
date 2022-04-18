42185417b3b43aa3ce9117f710c1ee2293a1b331
zagz16 <varkutinov90@mail.ru>
2022-04-18 23:38:53 +0200

    first commit

---
+++ 01-cards-sources/app.js
@@ -0,0 +1,24 @@
+
+
+function slidesPlugin(activeSlides) {
+    const slides = document.querySelectorAll(".slide")
+
+    slides[activeSlides].classList.add("active")
+
+    for (const slide of slides) {
+        slide.addEventListener("click", () => {
+            clearActiveClasses()
+
+            slide.classList.add("active")
+        })
+    }
+
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove("active")
+        })
+    }
+}
+
+slidesPlugin(3)
